c20ef5f4d28966f89c96c8dc1f16b0182d541a92
Shivam Varshney <12111043@nitkkr.ac.in>
2024-03-04 17:12:26 +0530

    Home page Change

--- lib/mvvm/views/HomeView/home.dart
+++ lib/mvvm/views/HomeView/home.dart
@@ -12,6 +12,13 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: const Center(
+        child: Text('Home'),
+      ),
+    );
   }
 }
