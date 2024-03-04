517c40b8c3b313e01a27ef947311ba8ed26e591c
jigovind556 <12113078@nitkkr.ac.in>
2024-03-04 21:15:05 +0530

    added firebase with some bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -16,7 +19,14 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-
+      home: Scaffold(
+        appBar: AppBar(title: Text('Bookish')),
+        body: Center(
+          child: Column(
+            children: [Text("helllo govind ji  "), Text("hello Anshu bro")],
+          ),
+        ),
+      ),
     );
   }
 }
