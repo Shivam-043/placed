97d48b12d6df1ac5d5c8c115aaa7720412366839
Shivam Varshney <12111043@nitkkr.ac.in>
2024-03-06 13:16:58 +0530

    Added Responsive Sizer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:placed/utils/firebase/firebase_configure.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,24 +13,23 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Place It',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Scaffold(
-        appBar: AppBar(title: Text('Bookish')),
-        body: Center(
-          child: Column(
-            children: [Text("helllo govind ji"), Text("hello Anshu bro")],
+    return ResponsiveSizer(builder: (context, orientation, deviceType) {
+      return MaterialApp(
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: Scaffold(
+          appBar: AppBar(title: Text('Bookish')),
+          body: Center(
+            child: Column(
+              children: [Text("helllo govind ji"), Text("hello Anshu bro")],
+            ),
           ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
